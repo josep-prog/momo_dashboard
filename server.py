5f4428ba9ac51414b99f46630a382920afeb5399
Justineneema <j.neema@alustudent.com>
2025-06-14 14:56:48 +0200

    modified

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, request, send_from_directory
 import sqlite3
-# from db import get_unique_names
-# import traceback
-# import os
+from db import get_unique_names
+import traceback
+import os
 app = Flask(__name__)
 DB_PATH = 'corrected_data.db'
 
